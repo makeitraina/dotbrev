50447f73b53b1d1275be82f6c5bd780f16c11196
Neil Raina <mrneilraina@gmail.com>
2021-08-08 17:32:32 -0400

    install yarn, node, golang and vscode extensions

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -4,21 +4,46 @@
 ##### Specify software and dependencies that are required for this project #####
 ################################################################################
 
+##### Update and upgrade packages #####
+sudo apt-get update
+sudo apt-get upgrade -y
+
 ##### Yarn #####
-# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add
-# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-# sudo apt update
-# sudo apt install -y yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install -y yarn
 
-##### Node v14.x + npm #####
-# curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-# sudo apt-get install -y nodejs
+##### Node v12.x + npm #####
+curl -fsSL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-##### Python + Pip + Poetry #####
-# sudo apt-get install -y python3-distutils
-# sudo apt-get install -y python3-apt
-# curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-# python3 get-pip.py
-# rm get-pip.py
-# source $HOME/.poetry/env
+##### Intall golang 14.x #####
+wget https://golang.org/dl/go1.14.14.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.7.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+
+##### Install vs-code extensions #####
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension donjayamanne.githistory
+code --install-extension eriklynd.json-tools
+code --install-extension esbenp.prettier-vscode
+code --install-extension GitHub.copilot
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension golang.go
+code --install-extension mariomatheu.syntax-project-pbxproj
+code --install-extension mark-tucker.aws-cli-configure
+code --install-extension mikestead.dotenv
+code --install-extension mohsen1.prettify-json
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension mziyabo.vscode-codedeploy
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension tberman.json-schema-validator
+code --install-extension ThreadHeap.serverless-ide-vscode
+code --install-extension TimVaneker.serverless-snippets
+code --install-extension zhuangtongfa.material-theme
